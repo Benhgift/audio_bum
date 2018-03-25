ecb48130f0b0b9a96119c58f79790daa45f96f85
Your Name <you@example.com>
2018-03-24 21:05:43 -0700

    first, we can read audio kinda

---
+++ hi.py
@@ -0,0 +1,32 @@
+import pdir
+from pdb import set_trace
+import pyaudio
+import wave
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 4100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+    #set_trace()
+    print(data[:20])
+    print()
+
+print(len(frames))
